17f45a0734b19c60bd20658d5f61b96e235bce7b
Nagy István <>
2020-12-05 10:08:37 +0100

    init

---
+++ example/android/app/src/main/java/com/mapbox/example/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.mapbox.example
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+    }
+}
